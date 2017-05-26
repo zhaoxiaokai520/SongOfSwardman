6941a670e64b8fcde587efa3b21fcd3fffe8728e
zhaoxiaokai <zhaoxiaokai520@outlook.com>
2017-05-26 08:30:14 +0800

    add UpdateGameMgr

---
+++ Assets/Scripts/UI/Base/IUniform.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts.Role;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.UI.Base
+{
+    //used to substitute mono Update method to speed up life method calls
+    public interface IUniform
+    {
+        void FastUpdate(float delta);
+
+        void Spawn(ActorRoot owner);
+    }
+}
