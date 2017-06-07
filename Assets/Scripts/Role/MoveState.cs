8e9c594cd4b928705d0b60fec9d38e225dfbb738
zhaoxiaokai <zhaoxiaokai520@outlook.com>
2017-06-07 08:14:22 +0800

    add movestate file

---
+++ Assets/Scripts/Role/MoveState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Role
+{
+    class MoveState
+    {
+        public enum Status{ IDLE, WALK, RUN, TELEPORT, JUMP, CLAMP};
+    }
+}
