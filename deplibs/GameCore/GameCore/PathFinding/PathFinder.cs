2bd5a36d7dfc3cbb74204690cf1a34e4e51489a2
zhaoxiaokai <zhaoxiaokai520@outlook.com>
2017-10-11 08:40:42 +0800

    change path finder interface

--- deplibs/GameCore/GameCore/PathFinding/PathFinder.cs
+++ deplibs/GameCore/GameCore/PathFinding/PathFinder.cs
@@ -3,46 +3,78 @@ using System.Runtime.InteropServices;
 
 public class PathFinder
 {
-    //#region cpp bridge interface
-    //[DllImport("GameCoreCpp.dll", EntryPoint = "ReqPath", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
-    //public static extern void ReqPath(VecInt2 from, VecInt2 goal);
+    #region cpp bridge interface
+#if UNITY_EDITOR || UNITY_STANDALONE_WIN
+    delegate void ReqPathP(VecInt2 from, VecInt2 goal);
+    delegate void ReqPathC(VecInt2 from, VecInt2 center, int radius);
+    delegate void ReqPathR(VecInt2 from, RECT goal);
+    delegate void ReqPathInvertC(VecInt2 from, VecInt2 center, int radius);
+    delegate void ReqPathInvertR(VecInt2 from, RECT goal);
+#else
+    [DllImport("GameCoreCpp.dll", EntryPoint = "ReqPathP", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void ReqPathP(VecInt2 from, VecInt2 goal);
 
-    //[DllImport("GameCoreCpp.dll", EntryPoint = "ReqPath", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
-    //public static extern void ReqPath(VecInt2 from, VecInt2 center, int radius);
+    [DllImport("GameCoreCpp.dll", EntryPoint = "ReqPathC", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void ReqPathC(VecInt2 from, VecInt2 center, int radius);
 
-    //[DllImport("GameCoreCpp.dll", EntryPoint = "ReqPath", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
-    //public static extern void ReqPath(VecInt2 from, RECT goal);
+    [DllImport("GameCoreCpp.dll", EntryPoint = "ReqPathR", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void ReqPathR(VecInt2 from, RECT goal);
 
-    //[DllImport("GameCoreCpp.dll", EntryPoint = "ReqPathInvert", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
-    //public static extern void ReqPathInvert(VecInt2 from, VecInt2 center, int radius);
+    [DllImport("GameCoreCpp.dll", EntryPoint = "ReqPathInvertC", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void ReqPathInvertC(VecInt2 from, VecInt2 center, int radius);
 
-    //[DllImport("GameCoreCpp.dll", EntryPoint = "ReqPathInvert", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
-    //public static extern void ReqPathInvert(VecInt2 from, RECT goal);
-    //#endregion
+    [DllImport("GameCoreCpp.dll", EntryPoint = "ReqPathInvertR", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void ReqPathInvertR(VecInt2 from, RECT goal);
+#endif
+    #endregion
 
-    //void RequestPath(VecInt2 from, VecInt2 goal)
-    //{
-    //    ReqPath(from, goal);
-    //}
-    ////in circle
-    //void RequestPath(VecInt2 from, VecInt2 center, int radius)
-    //{
-    //    ReqPath(from, center, radius);
-    //}
-    ////square
-    //void RequestPath(VecInt2 from, RECT rect)
-    //{
-    //    ReqPath(from, rect);
-    //}
-    ////inverted circle
-    //void RequestPathInvert(VecInt2 from, VecInt2 center, int radius)
-    //{
-    //    ReqPathInvert(from, center, radius);
-    //}
+    void RequestPath(VecInt2 from, VecInt2 goal)
+    {
+#if UNITY_EDITOR || UNITY_STANDALONE_WIN
+        NativePluginHelper.Invoke<ReqPathP>(NativePluginHelper.nativeLibraryPtr, from, goal);
+#else
+        ReqPathP(from, goal);
+#endif
 
-    ////inverted square
-    //void RequestPathInvert(VecInt2 from, RECT rect)
-    //{
-    //    ReqPathInvert(from, rect);
-    //}
+    }
+    //in circle
+    void RequestPath(VecInt2 from, VecInt2 center, int radius)
+    {
+#if UNITY_EDITOR || UNITY_STANDALONE_WIN
+        NativePluginHelper.Invoke<ReqPathC>(NativePluginHelper.nativeLibraryPtr, from, center, radius);
+#else
+        ReqPathC(from, center, radius);
+#endif
+
+    }
+    //square
+    void RequestPath(VecInt2 from, RECT rect)
+    {
+#if UNITY_EDITOR || UNITY_STANDALONE_WIN
+        NativePluginHelper.Invoke<ReqPathR>(NativePluginHelper.nativeLibraryPtr, from, rect);
+#else
+        ReqPathR(from, rect);
+#endif
+
+    }
+    //inverted circle
+    void RequestPathInvert(VecInt2 from, VecInt2 center, int radius)
+    {
+#if UNITY_EDITOR || UNITY_STANDALONE_WIN
+        NativePluginHelper.Invoke<ReqPathInvertC>(NativePluginHelper.nativeLibraryPtr, from, center, radius);
+#else
+        ReqPathInvertC(from, center, radius);
+#endif
+
+    }
+
+    //inverted square
+    void RequestPathInvert(VecInt2 from, RECT rect)
+    {
+#if UNITY_EDITOR || UNITY_STANDALONE_WIN
+        NativePluginHelper.Invoke<ReqPathInvertR>(NativePluginHelper.nativeLibraryPtr, from, rect);
+#else
+        ReqPathInvertR(from, rect);
+#endif
+    }
 }
